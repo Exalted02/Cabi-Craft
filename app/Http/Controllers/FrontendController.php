275d1b41e115077ea78e4a5afee10f7860761e7b
Exalted02 <exaltedsol02@gmail.com>
2024-08-06 11:30:42 +0530

    up

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class FrontendController extends Controller
 {	
 	public function index()
 	{
+		dd(Category::get());
 		return view('home');
 	}
 	public function browse()
